7fa2978a21224c9f979f708c018ea13f29a0da8a
Lavda <kaustubhwahane55@gmail.com>
2024-03-22 19:23:05 +0530

    Logo Added

---
+++ src/components/Classes/Classes.jsx
@@ -0,0 +1,8 @@
+
+export default function Classes() {
+  return (
+    <div>
+        <h1>Hello classses</h1>
+    </div>
+  )
+}
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons'; 
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'; 
+import Logo from '../../assets/karoabhyasLogo.png';
 import { useState } from 'react';
 import './Navigation.css';
+
 
 function Navigation() {
   const [activeItem, setActiveItem] = useState(null); 
@@ -12,17 +14,24 @@ function Navigation() {
   };
   
   return (
+    <>
     <nav className="navigation">
+    <div className="logo-container">
       <h1 className="title">KaroAbhayaas</h1>
-      <ul className="nav-list">
-        <li><button className={`button ${activeItem === 0 ? 'black' : ''}`} onClick={() => handleItemClick(0)}>Join Us</button></li>
-        <li><a className={`nav-item ${activeItem === 1 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(1)}>Classes</a></li>
-        <li><a className={`nav-item ${activeItem === 2 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(2)}>Products</a></li>
-        <li><a className={`nav-item ${activeItem === 3 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(3)}>About Us</a></li>
-        <li><a className={`nav-item ${activeItem === 4 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(4)}><FontAwesomeIcon icon={faUser} /></a></li>
-        <li><a className={`nav-item ${activeItem === 5 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(5)}><FontAwesomeIcon icon={faShoppingCart} /></a></li>
-      </ul>
-    </nav>
+      <img src={Logo} alt="Error in Logo" className='logocss' />
+    </div>
+    <ul className="nav-list">
+      <li><button className={`button ${activeItem === 0 ? 'black' : ''}`} onClick={() => handleItemClick(0)}>Join Us</button></li>
+      <li><a className={`nav-item ${activeItem === 1 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(1)}>Classes</a></li>
+      <li><a className={`nav-item ${activeItem === 2 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(2)}>Products</a></li>
+      <li><a className={`nav-item ${activeItem === 3 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(3)}>About Us</a></li>
+      <li><a className={`nav-item ${activeItem === 4 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(4)}><FontAwesomeIcon icon={faUser} /></a></li>
+      <li><a className={`nav-item ${activeItem === 5 ? 'black' : ''}`} href="#" onClick={() => handleItemClick(5)}><FontAwesomeIcon icon={faShoppingCart} /></a></li>
+    </ul>
+  </nav>
+  <hr style={{opacity: 0.3}}/>
+  </>
+
   );
 }
 
