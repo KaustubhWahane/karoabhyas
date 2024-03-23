b9cc84760463ad3b1fffa61cf4f354ba3573aced
Lavda <kaustubhwahane55@gmail.com>
2024-03-23 20:17:33 +0530

    Recheck

--- src/components/Home/Footer/Footer.jsx
+++ src/components/Home/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
             <p>Karo Abhyas <br /> 112, ELECTRONIC CITY <br /> BANGALORE, INDIA</p>
         </div>
         <div className="right-text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quasi. Aliquid illum et incidunt veniam.</p>    
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quod, quasi. Aliquid illum et incidunt veniam.</p>    
             <button className="custom-button">Click me</button>
             <button className="custom-button">About</button>
         </div>
