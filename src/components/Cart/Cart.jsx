dff454adb23ffaccf8e71e3b565a5b0eb51c4ec2
Lavda <kaustubhwahane55@gmail.com>
2024-03-23 21:04:49 +0530

    Cart in process

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,33 @@
+import "./Cart.css";
 
-export default function Cart() {
+function Cart() {
   return (
-    <div>
-        <h1>Cart</h1>
-        
+    <div className="cart">
+      <div className="main-container">
+        <div className="left-side">Total: 2000 Rupees</div>
+        <div className="right-side">
+          <button className="checkout-button">Checkout</button>
+        </div>
+        <div className="question-container">
+          <div className="question">
+            <div className="left-side">
+              <div className="question-title">Question Title</div>
+              <div className="module">Module: NAME OF MODULE</div>
+              <div className="date">Date: 24/04/2024</div>
+            </div>
+            <div className="right-side">
+              <select className="dropdown">
+                {[...Array(10).keys()].map(num => (
+                  <option key={num + 1} value={num + 1}>{num + 1}</option>
+                ))}
+              </select>
+              <button className="delete-button">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+export default Cart;
