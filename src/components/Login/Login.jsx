51807f94dbcaf48afd878f44bc31c1e9d512552b
Lavda <kaustubhwahane55@gmail.com>
2024-03-24 15:03:41 +0530

    LoginAccess

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
 import './Login.css'; 
+import LoginAccess from './LoginAccess'; // Import the LoginAccess component
 
 function Login() {
   const [showSignUp, setShowSignUp] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    alert('Just to check its working!');
-  };
-
-  const handleSignUp = () => {
-    alert('Just to check its working!');
+    setLoggedIn(true); // Change login state to true when login button be clicked
   };
 
   const toggleSignUp = () => {
     setShowSignUp(!showSignUp);
   };
+
+  // Render the LoginAccess component if the user be logged in
+  if (loggedIn) {
+    return <LoginAccess />;
+  }
 
   return (
     <div className="login-container">
@@ -48,8 +51,8 @@ function Login() {
                 className="form-input" 
                 size={20}
               />
-              <button type="button" onClick={handleSignUp} className="login-button">Send OTP</button>
-              <p>Didn&apos;t recieve it? <a href="#" style={{textDecoration:"none"}}>RESEND</a></p>
+              <button type="button" className="login-button">Send OTP</button>
+              <p>Didn&apos;t receive it? <a href="#" style={{textDecoration:"none"}}>RESEND</a></p>
             </div>
             <div className="form-group">
               <label htmlFor="otp" className="form-label">OTP:</label>
@@ -60,7 +63,7 @@ function Login() {
                 size={30} 
               />
             </div>
-            <button type="button" onClick={handleSignUp} className="login-button">Sign Up</button>
+            <button type="button" className="login-button">Sign Up</button>
           </>
         ) : (
           <>
---
+++ src/components/Login/LoginAccess.jsx
@@ -0,0 +1,18 @@
+function LoginAccess() {
+  return (
+    <div className="container">
+      <div className="left-side">
+        <p>
+            Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+      <div className="right-side">
+        <p>
+            Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default LoginAccess;
